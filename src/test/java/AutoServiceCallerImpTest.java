8c9085ea1a63efbe5f37401915f7c9807dc39468
Mervine, Jonathan <jon.mervine@gmail.com>
2019-05-22 15:29:11 -0400

    Fixing Unit Test Updating Pom and removing the vulnerable dependency as well as unused dependencies.

--- src/test/java/AutoServiceCallerImpTest.java
+++ src/test/java/AutoServiceCallerImpTest.java
@@ -4,13 +4,20 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.inject.Inject;
-
 /**
  * Created by mervinej on 4/12/2017.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class AutoServiceCallerImpTest {
+
+    @Mock
+    private AutoService bmwAutoService;
+
+    @Mock
+    private AutoService fordAutoService;
+
+    @Mock
+    private AutoService hondaAutoService;
 
     @InjectMocks
     private AutoServiceCallerImp autoServiceAllerImp;
